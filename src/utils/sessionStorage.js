56639e5fc61ed8cfb6d439c879f3f055c19d9ba8
Luiz Henrique Ricci Aureliano <luizaureliano.tds1.senai@gmail.com>
2025-05-15 14:34:04 -0300

    fix: edit req get

--- src/utils/sessionStorage.js
+++ src/utils/sessionStorage.js
@@ -2,15 +2,14 @@ const isClient = typeof window !== 'undefined';
 
 export const getSessionStorage = (key, initialValue) => {
     if (!isClient) return initialValue;
-    
+
     const stored = sessionStorage.getItem(key);
 
-    return stored ? JSON.parse(stored)  : initialValue;
-}
-
+    return stored ? JSON.parse(stored) : initialValue;
+};
 
 export const setSessionStorage = (key, value) => {
-    if (isClient){
-        setSessionStorage.setItem(key, JSON.stringify(value));
+    if (isClient) {
+        sessionStorage.setItem(key, JSON.stringify(value))
     }
-}
+};  
